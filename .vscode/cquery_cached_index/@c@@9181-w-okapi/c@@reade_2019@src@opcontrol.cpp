1f236ade1a98e1cccaf58d92bfc58f16c1ab5172
9181-W <readcaml@gmail.com>
2019-10-14 20:34:40 -0700

    9181W .vscode directory

---
+++ .vscode/cquery_cached_index/@c@@9181-w-okapi/c@@reade_2019@src@opcontrol.cpp
@@ -0,0 +1,272 @@
+#include "main.h"
+
+//void __sync_synchronize(void) {
+//    __sync_synchronize();
+//}
+extern "C" void __sync_synchronize() {}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.5i
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+#define LEFT_FRONT_WHEEL_PORT 1
+#define LEFT_REAR_WHEEL_PORT 11
+#define RIGHT_FRONT_WHEEL_PORT 10
+#define RIGHT_REAR_WHEEL_PORT 20
+#define LEFT_ARM_MOTOR_PORT 15
+#define RIGHT_ARM_MOTOR_PORT 16
+#define LEFT_INTAKE_MOTOR_PORT 8
+#define RIGHT_INTAKE_MOTOR_PORT 2
+#define SCALE 1.0
+
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::Motor lf_mtr(LEFT_FRONT_WHEEL_PORT);
+pros::Motor lr_mtr(LEFT_REAR_WHEEL_PORT);
+pros::Motor rf_mtr(RIGHT_FRONT_WHEEL_PORT, true);
+pros::Motor rr_mtr(RIGHT_REAR_WHEEL_PORT, true);
+pros::Motor la_mtr(LEFT_ARM_MOTOR_PORT, pros::E_MOTOR_GEARSET_36);
+pros::Motor ra_mtr(RIGHT_ARM_MOTOR_PORT, pros::E_MOTOR_GEARSET_36, true);
+pros::Motor li_mtr(LEFT_INTAKE_MOTOR_PORT);
+pros::Motor ri_mtr(RIGHT_INTAKE_MOTOR_PORT, true);
+
+// Forward declarations
+void arcade_control_1();
+void arcade_control_2();
+void arcade_control_3();
+void tank_control();
+
+void opcontrol()
+{
+
+  lf_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+  lr_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+  rf_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+  rr_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+  la_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+  ra_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+  li_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+  ri_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+
+	while (true)
+	{
+    //arcade_control_1();
+	  //arcade_control_2();
+	  arcade_control_3();
+	  //tank_control();
+
+		//Use buttons to make intake move
+		if (master.get_digital(DIGITAL_R1) == 1)
+		{
+			li_mtr.move(127);
+			ri_mtr.move(127);
+		}
+		else if (master.get_digital(DIGITAL_L1) == 1)
+		{
+			li_mtr.move(-40);
+			ri_mtr.move(-40);
+		}
+		else
+		{
+			li_mtr.move(0);
+			ri_mtr.move(0);
+		}
+
+		// make arm move
+		if (master.get_digital(DIGITAL_R2) == 1)
+		{
+			ra_mtr.move(45);
+			la_mtr.move(45);
+		}
+		else if (master.get_digital(DIGITAL_L2) == 1)
+		{
+			ra_mtr.move(-70);
+			la_mtr.move(-70);
+		}
+		else
+		{
+			ra_mtr.move(0);
+			la_mtr.move(0);
+		}
+
+    if (master.get_digital(DIGITAL_B))
+    {
+      la_mtr.move(-127);
+      ra_mtr.move(-127);
+    }
+
+    //Use Y button to open cartridge
+    static bool do_once = true;
+    if ((master.get_digital(DIGITAL_Y) == 1) && do_once)
+    {
+      la_mtr.move(127);
+      ra_mtr.move(127);
+      pros::delay(400);
+      ra_mtr.move(0);
+      la_mtr.move(0);
+      pros::delay(400);
+      la_mtr.move(-127);
+      ra_mtr.move(-127);
+      pros::delay(400);
+      ra_mtr.move(0);
+      la_mtr.move(0);
+      do_once = false;
+    }
+
+		//Use buttons to set brake mode or coast
+		if (master.get_digital(DIGITAL_RIGHT) == 1)
+		{
+			printf("Press Right");
+			lf_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+			rf_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+			lr_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+			rr_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+			lf_mtr.move(0);
+			lr_mtr.move(0);
+			rf_mtr.move(0);
+			rr_mtr.move(0);
+		}
+    else
+    {
+      printf("Right not pressed");
+    }
+		if (master.get_digital(DIGITAL_DOWN) == 1)
+		{
+			printf("Press DOWN");
+			lf_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+			rf_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+			lr_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+			rr_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+			lf_mtr.move(0);
+			lr_mtr.move(0);
+			rf_mtr.move(0);
+			rr_mtr.move(0);
+		}
+
+		pros::delay(2);
+	}
+}
+
+void arcade_control_1()
+{
+  int power = master.get_analog(ANALOG_LEFT_Y);
+  int turn = master.get_analog(ANALOG_RIGHT_X);
+  int left = power;
+  int right = power;
+
+	if (turn < -64)
+	{
+		left = power * -1;
+	}
+
+	if (turn > 64)
+	{
+		right = power * -1;
+	}
+
+	left = left * SCALE;
+	right = right * SCALE;
+
+  lf_mtr.move(left);
+  lr_mtr.move(left);
+	rf_mtr.move(right);
+	rr_mtr.move(right);
+}
+
+
+void arcade_control_2()
+{
+	int power = master.get_analog(ANALOG_LEFT_Y);
+	int turn = master.get_analog(ANALOG_RIGHT_X);
+	int left = power + turn;
+	int right = power - turn;
+
+  if (left > 127)
+	{
+	  left = 127;
+	}
+
+	if (left < -127)
+	{
+		left = -127;
+	}
+
+	if (right > 127)
+	{
+		right = 127;
+	}
+
+	if (right < -127)
+	{
+		right = -127;
+	}
+
+	left = left * SCALE;
+	right = right * SCALE;
+
+	lf_mtr.move(left);
+	lr_mtr.move(left);
+	rf_mtr.move(right);
+	rr_mtr.move(right);
+}
+
+
+void arcade_control_3()
+{
+	int power = master.get_analog(ANALOG_LEFT_Y);
+	int turn = master.get_analog(ANALOG_LEFT_X);
+	int left = power + turn;
+	int right = power - turn;
+
+  if (left > 127)
+	{
+	  left = 127;
+	}
+
+	if (left < -127)
+	{
+		left = -127;
+	}
+
+	if (right > 127)
+	{
+		right = 127;
+	}
+
+	if (right < -127)
+	{
+		right = -127;
+	}
+
+	left = left * SCALE;
+	right = right * SCALE;
+
+	lf_mtr.move(left);
+	lr_mtr.move(left);
+	rf_mtr.move(right);
+	rr_mtr.move(right);
+}
+
+
+void tank_control()
+{
+	// Read the controller
+  float left = master.get_analog(ANALOG_LEFT_Y);
+	float right = master.get_analog(ANALOG_RIGHT_Y);
+
+  // Set the speed of the motors.
+	left = left * SCALE;
+	right = right * SCALE;
+  lf_mtr.move(left);
+	lr_mtr.move(left);
+	rf_mtr.move(right);
+  rr_mtr.move(right);
+}
---
+++ .vscode/cquery_cached_index/c@@9181-w-okapi/src@autonomous.cpp
@@ -0,0 +1,291 @@
+#include "main.h"
+#include "okapi/api.hpp"
+using namespace okapi;
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+
+#define LEFT_FRONT_WHEEL_PORT 1
+#define LEFT_REAR_WHEEL_PORT 11
+#define RIGHT_FRONT_WHEEL_PORT 10
+#define RIGHT_REAR_WHEEL_PORT 20
+#define LEFT_ARM_MOTOR_PORT 15
+#define RIGHT_ARM_MOTOR_PORT 16
+#define LEFT_INTAKE_MOTOR_PORT 8
+#define RIGHT_INTAKE_MOTOR_PORT 2
+
+#define MEGA_MAX_SPEED (100.0)
+#define MAX_SPEED (80.0)
+#define THREE_QUARTER_SPEED (60.0)
+#define HALF_SPEED (40.0)
+#define SLOW_SPEED (20.0)
+
+#define LIFT_POSITION_BOTTOM 0.0
+#define LIFT_POSITION_MIDDLE 2000.0
+#define LIFT_POSITION_TOP    4000.0
+
+extern pros::Motor la_mtr;
+extern pros::Motor ra_mtr;
+extern pros::Motor li_mtr;
+extern pros::Motor ri_mtr;
+
+void intake_on(double speed = 200.0)
+{
+  li_mtr.move_velocity(speed);
+  ri_mtr.move_velocity(speed);
+}
+void intake_off()
+{
+  li_mtr.move_velocity(0);
+  ri_mtr.move_velocity(0);
+}
+
+void autonomous()
+{
+  // Create controllers
+  auto chassis = ChassisControllerFactory::create(
+    {LEFT_FRONT_WHEEL_PORT, LEFT_REAR_WHEEL_PORT},
+    {-RIGHT_FRONT_WHEEL_PORT, -RIGHT_REAR_WHEEL_PORT},
+    IterativePosPIDController::Gains{0.005, 0, 0.0001},
+    IterativePosPIDController::Gains{0.005, 0, 0.0001},
+    IterativePosPIDController::Gains{0.005, 0, 0.0001},
+    AbstractMotor::gearset::green,
+    {4.0_in, 14.375_in}
+  );
+
+  pros::Motor la_mtr(LEFT_ARM_MOTOR_PORT, pros::E_MOTOR_GEARSET_36);
+  pros::Motor ra_mtr(RIGHT_ARM_MOTOR_PORT, pros::E_MOTOR_GEARSET_36, true);
+  pros::Motor li_mtr(LEFT_INTAKE_MOTOR_PORT);
+  pros::Motor ri_mtr(RIGHT_INTAKE_MOTOR_PORT, true);
+
+  la_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+  ra_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+  li_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+  ri_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+
+  /*
+  chassis.setMaxVelocity(MAX_SPEED);
+  chassis.moveDistanceAsync(42_in);
+  intake_on(200);
+  chassis.waitUntilSettled();
+  intake_off();
+  chassis.setMaxVelocity(HALF_SPEED);
+  chassis.turnAngle(35_deg);
+  chassis.setMaxVelocity(MAX_SPEED);
+  chassis.moveDistanceAsync(8.5_in);
+  intake_on(200);
+  chassis.waitUntilSettled();
+  intake_off();
+  chassis.setMaxVelocity(HALF_SPEED);
+  chassis.turnAngle(40_deg);
+  chassis.setMaxVelocity(MAX_SPEED);
+  chassis.moveDistanceAsync(-7.4_in);
+  intake_on(200);
+  chassis.waitUntilSettled();
+  intake_on(200);
+  pros::delay(200);
+  intake_off();
+  pros::delay(400);
+  chassis.setMaxVelocity(HALF_SPEED);
+  chassis.turnAngle(-53_deg);
+  chassis.setMaxVelocity(MAX_SPEED);
+  chassis.moveDistanceAsync(10.5_in);
+  intake_on(154);
+  chassis.waitUntilSettled();
+  intake_off();
+  chassis.moveDistanceAsync(-9_in);
+  chassis.waitUntilSettled();
+  chassis.setMaxVelocity(HALF_SPEED);
+  chassis.turnAngle(-173_deg);
+  chassis.setMaxVelocity(MAX_SPEED);
+  intake_on(180);
+  pros::delay(1600);
+  intake_off();
+  chassis.moveDistanceAsync(13_in);
+  chassis.waitUntilSettled();
+  chassis.setMaxVelocity(HALF_SPEED);
+  chassis.turnAngle(-115_deg);
+  chassis.setMaxVelocity(MAX_SPEED);
+  chassis.moveDistanceAsync(18.5_in);
+  intake_on(200);
+  chassis.waitUntilSettled();
+  intake_off();
+  */
+
+// Skills
+/*
+  chassis.setMaxVelocity(MAX_SPEED);
+  la_mtr.move(127);
+  ra_mtr.move(127);
+  pros::delay(400);
+  ra_mtr.move(0);
+  la_mtr.move(0);
+  pros::delay(400);
+  la_mtr.move(-127);
+  ra_mtr.move(-127);
+  pros::delay(400);
+  ra_mtr.move(0);
+  la_mtr.move(0);
+  chassis.moveDistanceAsync(42_in);
+  li_mtr.move(200);
+  ri_mtr.move(200);
+  chassis.waitUntilSettled();
+  li_mtr.move(0);
+  ri_mtr.move(0);
+  chassis.setMaxVelocity(HALF_SPEED);
+  chassis.turnAngle(35_deg);
+  chassis.setMaxVelocity(MAX_SPEED);
+  chassis.moveDistanceAsync(8.5_in);
+  li_mtr.move(160);
+  ri_mtr.move(160);
+  chassis.waitUntilSettled();
+  li_mtr.move(-75);
+  ri_mtr.move(-75);
+  pros::delay(100);
+  li_mtr.move(100);
+  ri_mtr.move(100);
+  pros::delay(100);
+  li_mtr.move(0);
+  ri_mtr.move(0);
+  chassis.setMaxVelocity(HALF_SPEED);
+  chassis.turnAngle(40_deg);
+  chassis.setMaxVelocity(MAX_SPEED);
+  chassis.moveDistanceAsync(-10.4_in);
+  li_mtr.move(200);
+  ri_mtr.move(200);
+  chassis.waitUntilSettled();
+  li_mtr.move(200);
+  ri_mtr.move(200);
+  pros::delay(200);
+  li_mtr.move(0);
+  ri_mtr.move(0);
+  pros::delay(400);
+  chassis.setMaxVelocity(HALF_SPEED);
+  chassis.turnAngle(-48_deg);
+  chassis.setMaxVelocity(MAX_SPEED);
+  chassis.moveDistanceAsync(13_in);
+  li_mtr.move(150);
+  ri_mtr.move(150);
+  chassis.waitUntilSettled();
+  li_mtr.move(0);
+  ri_mtr.move(0);
+  li_mtr.move(-75);
+  ri_mtr.move(-75);
+  pros::delay(100);
+  li_mtr.move(100);
+  ri_mtr.move(100);
+  pros::delay(100);
+  li_mtr.move(0);
+  ri_mtr.move(0);
+  chassis.moveDistanceAsync(-9_in);
+  chassis.waitUntilSettled();
+  chassis.setMaxVelocity(HALF_SPEED);
+  chassis.turnAngle(-173_deg);
+  chassis.setMaxVelocity(MAX_SPEED);
+  li_mtr.move(180);
+  ri_mtr.move(180);
+  pros::delay(1600);
+  li_mtr.move(0);
+  ri_mtr.move(0);
+  chassis.setMaxVelocity(HALF_SPEED);
+  chassis.turnAngle(-35_deg);
+  chassis.setMaxVelocity(MAX_SPEED);
+  chassis.moveDistanceAsync(15_in);
+  chassis.waitUntilSettled();
+  chassis.setMaxVelocity(HALF_SPEED);
+  chassis.turnAngle(-103_deg);
+  chassis.setMaxVelocity(MAX_SPEED);
+  chassis.moveDistanceAsync(19_in);
+  li_mtr.move(200);
+  ri_mtr.move(200);
+  chassis.waitUntilSettled();
+  li_mtr.move(0);
+  ri_mtr.move(0);
+  li_mtr.move(-75);
+  ri_mtr.move(-75);
+  pros::delay(100);
+  li_mtr.move(100);
+  ri_mtr.move(100);
+  pros::delay(100);
+  li_mtr.move(0);
+  ri_mtr.move(0);
+  */
+
+  //Back Blue
+  chassis.setMaxVelocity(MAX_SPEED);
+  la_mtr.move(127);
+  ra_mtr.move(127);
+  pros::delay(400);
+  ra_mtr.move(0);
+  la_mtr.move(0);
+  pros::delay(400);
+  la_mtr.move(-127);
+  ra_mtr.move(-127);
+  pros::delay(400);
+  ra_mtr.move(0);
+  la_mtr.move(0);
+  pros::delay(200);
+  chassis.moveDistanceAsync(41_in);
+  li_mtr.move(160);
+  ri_mtr.move(160);
+  chassis.waitUntilSettled();
+  li_mtr.move(0);
+  ri_mtr.move(0);
+  chassis.setMaxVelocity(THREE_QUARTER_SPEED);
+  chassis.turnAngle(35_deg);
+  chassis.setMaxVelocity(MAX_SPEED);
+  chassis.moveDistanceAsync(10_in);
+  li_mtr.move(150);
+  ri_mtr.move(150);
+  chassis.waitUntilSettled();
+  li_mtr.move(-75);
+  ri_mtr.move(-75);
+  pros::delay(100);
+  li_mtr.move(100);
+  ri_mtr.move(100);
+  pros::delay(100);
+  li_mtr.move(0);
+  ri_mtr.move(0);
+  chassis.moveDistanceAsync(-20_in);
+  chassis.waitUntilSettled();
+  chassis.turnAngle(185_deg);
+  chassis.moveDistanceAsync(19_in);
+  li_mtr.move(-75);
+  ri_mtr.move(-75);
+  pros::delay(100);
+  li_mtr.move(100);
+  ri_mtr.move(100);
+  pros::delay(100);
+  li_mtr.move(0);
+  ri_mtr.move(0);
+  chassis.waitUntilSettled();
+  ra_mtr.move(80);
+  la_mtr.move(80);
+  pros::delay(400);
+  la_mtr.move(0);
+  ra_mtr.move(0);
+  pros::delay(200);
+  ra_mtr.move(80);
+  la_mtr.move(80);
+  pros::delay(1200);
+  la_mtr.move(0);
+  ra_mtr.move(0);
+  chassis.setMaxVelocity(HALF_SPEED);
+  chassis.moveDistanceAsync(4_in);
+  chassis.waitUntilSettled();
+  chassis.setMaxVelocity(MEGA_MAX_SPEED);
+  chassis.moveDistanceAsync(-10_in);
+  chassis.waitUntilSettled();
+
+
+
+}
